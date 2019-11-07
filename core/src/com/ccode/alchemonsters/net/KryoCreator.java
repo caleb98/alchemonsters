10ef1d7841650cb616cbee8116154c8797735913
Caleb <psychotix@hotmail.com>
2019-11-07 15:43:43 -0600

    added net package and KryoCreator class for easy creation of servers/clients and serialization class registration

---
+++ core/src/com/ccode/alchemonsters/net/KryoCreator.java
@@ -0,0 +1,62 @@
+package com.ccode.alchemonsters.net;
+
+import com.ccode.alchemonsters.engine.event.messages.MCombatAilmentApplied;
+import com.ccode.alchemonsters.engine.event.messages.MCombatAilmentRemoved;
+import com.ccode.alchemonsters.engine.event.messages.MCombatChargeFinished;
+import com.ccode.alchemonsters.engine.event.messages.MCombatChargeStarted;
+import com.ccode.alchemonsters.engine.event.messages.MCombatDamageDealt;
+import com.ccode.alchemonsters.engine.event.messages.MCombatFinished;
+import com.ccode.alchemonsters.engine.event.messages.MCombatGroundChanged;
+import com.ccode.alchemonsters.engine.event.messages.MCombatHealingReceived;
+import com.ccode.alchemonsters.engine.event.messages.MCombatStarted;
+import com.ccode.alchemonsters.engine.event.messages.MCombatStatBuffApplied;
+import com.ccode.alchemonsters.engine.event.messages.MCombatStateChanged;
+import com.ccode.alchemonsters.engine.event.messages.MCombatTeamActiveChanged;
+import com.ccode.alchemonsters.engine.event.messages.MCombatTerrainChanged;
+import com.ccode.alchemonsters.engine.event.messages.MCombatWeatherChanged;
+import com.ccode.alchemonsters.net.TestNet.TestNetChild;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.EndPoint;
+import com.esotericsoftware.kryonet.Server;
+
+public class KryoCreator {
+
+	private static void registerClasses(EndPoint endpoint) {
+		Kryo kryo = endpoint.getKryo();
+		
+		//Register all classes
+		kryo.register(MCombatAilmentApplied.class);
+		kryo.register(MCombatAilmentRemoved.class);
+		kryo.register(MCombatChargeFinished.class);
+		kryo.register(MCombatChargeStarted.class);
+		kryo.register(MCombatDamageDealt.class);
+		kryo.register(MCombatFinished.class);
+		kryo.register(MCombatGroundChanged.class);
+		kryo.register(MCombatHealingReceived.class);
+		kryo.register(MCombatStarted.class);
+		kryo.register(MCombatStatBuffApplied.class);
+		kryo.register(MCombatStateChanged.class);
+		kryo.register(MCombatTeamActiveChanged.class);
+		kryo.register(MCombatTerrainChanged.class);
+		kryo.register(MCombatWeatherChanged.class);
+		
+		//Network-specific classes
+		kryo.register(TestNet.class);
+		kryo.register(TestNetChild.class);
+		
+	}
+	
+	public static Server createServer() {
+		Server server = new Server();
+		registerClasses(server);
+		return server;
+	}
+	
+	public static Client createClient() {
+		Client client = new Client();
+		registerClasses(client);
+		return client;
+	}
+	
+}
--- server/src/com/ccode/alchemonsters/server/ServerLauncher.java
+++ server/src/com/ccode/alchemonsters/server/ServerLauncher.java
@@ -2,8 +2,8 @@ package com.ccode.alchemonsters.server;
 
 import java.io.IOException;
 
+import com.ccode.alchemonsters.net.KryoCreator;
 import com.esotericsoftware.kryonet.Connection;
-import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
 
 public class ServerLauncher {
@@ -12,27 +12,8 @@ public class ServerLauncher {
 	
 	public static void main (String[] arg) throws IOException {
 		
-		Server server = new Server();	
-		server.bind(48371, 48371);
-		server.addListener(new Listener(){
-			@Override
-			public void connected(Connection connection) {
-				System.out.printf("%s connected.\n", connection.getRemoteAddressUDP());
-				c = connection;
-			}
-			
-			@Override
-			public void disconnected(Connection connection) {
-				if(c == connection) {
-					System.out.println("yoo");
-				}
-				System.out.printf("%s disconnected.\n", connection.getRemoteAddressUDP());
-				System.out.println("Exiting...");
-				System.exit(0);
-			}
-		});
+		Server server = KryoCreator.createServer();
 		
-		server.start();
 		
 	}
 }
