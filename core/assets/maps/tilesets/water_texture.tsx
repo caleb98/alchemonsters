f8c4296fa2ff425d8a4048085835835062751ebb
caleb c <psychotix@hotmail.com>
2020-09-01 00:48:49 -0500

    Added temporary animated water texture

---
+++ core/assets/maps/tilesets/water_texture.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="water_texture" tilewidth="32" tileheight="32" tilecount="12" columns="12">
+ <image source="water_texture.png" width="384" height="32"/>
+ <tile id="11">
+  <animation>
+   <frame tileid="0" duration="180"/>
+   <frame tileid="1" duration="180"/>
+   <frame tileid="2" duration="180"/>
+   <frame tileid="3" duration="180"/>
+   <frame tileid="4" duration="180"/>
+   <frame tileid="5" duration="180"/>
+   <frame tileid="6" duration="180"/>
+   <frame tileid="7" duration="180"/>
+   <frame tileid="8" duration="180"/>
+   <frame tileid="9" duration="180"/>
+   <frame tileid="10" duration="180"/>
+   <frame tileid="11" duration="180"/>
+  </animation>
+ </tile>
+</tileset>
